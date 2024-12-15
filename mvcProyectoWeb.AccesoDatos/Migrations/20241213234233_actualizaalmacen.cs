228e226ebda129755cbb1b4de8508f5d50de2f63
Guillen-cloud <guillenbejaranor@gmail.com>
2024-12-15 17:23:43 -0400

    first commit

---
+++ mvcProyectoWeb.AccesoDatos/Migrations/20241213234233_actualizaalmacen.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace mvcProyectoWeb.AccesoDatos.Migrations
+{
+    /// <inheritdoc />
+    public partial class actualizaalmacen : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Orden",
+                table: "Almacen",
+                newName: "Numero");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Numero",
+                table: "Almacen",
+                newName: "Orden");
+        }
+    }
+}
